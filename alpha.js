b09d8a7f22d7fae9493a15212f0cae3d57e2a4ae
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-08-09 21:04:39 +0530

    Files added

--- alpha.js
+++ alpha.js
@@ -1,8 +1,8 @@
-const expree=require("express");
+const expree=require('express');
 const app=express();
 
 app.get(function(req,res){
-    res.sendFile(__dirname+"/index.html");
+    res.sendFile("index.html");
 });
 
 app.listen(3000);
